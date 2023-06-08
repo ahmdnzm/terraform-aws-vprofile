a5b67ea3b7650af513eeb58d327f3938cf3ba8b1
Ahmad Nizam <mohdrosnizam@gmail.com>
2023-06-08 13:05:30 +0800

    enable map_public_ip_on_launch to true

--- vpc.tf
+++ vpc.tf
@@ -7,10 +7,11 @@ module "vpc" {
   private_subnets = [var.PrivSub1CIDR, var.PrivSub2CIDR, var.PrivSub3CIDR]
   public_subnets  = [var.PubSub1CIDR, var.PubSub2CIDR, var.PubSub3CIDR]
 
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+  enable_nat_gateway      = true
+  single_nat_gateway      = true
+  enable_dns_hostnames    = true
+  enable_dns_support      = true
+  map_public_ip_on_launch = true
 
   tags = {
     Terraform   = "true"
